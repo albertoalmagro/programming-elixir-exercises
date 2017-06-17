453ff2dd59674fcb693af996e5bf73042956a837
Alberto Almagro <alberto.almagro@rakuten.com>
2017-06-18 00:49:14 +0200

    Add capitalize sentences exercise

---
+++ strings/capitalize_sentences.exs
@@ -0,0 +1,10 @@
+defmodule Utf8 do
+  def capitalize_sentences(str) when is_binary(str), do: _capitalize_sentences(str)
+
+  defp _capitalize_sentences(str) do
+    str
+    |> String.split(". ")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join(". ")
+  end
+end
