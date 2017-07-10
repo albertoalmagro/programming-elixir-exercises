cdda5d59db681cd34ea8385faf4330f9d506582e
Alberto Almagro <alberto.almagro@rakuten.com>
2017-07-11 00:27:41 +0200

    Add external API to Stack.Server

--- otp-server/1/stack/lib/stack/server.ex
+++ otp-server/1/stack/lib/stack/server.ex
@@ -1,6 +1,24 @@
 defmodule Stack.Server do
   use GenServer
 
+  #####
+  # External API
+
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(element) do
+    GenServer.cast(__MODULE__, { :push, element })
+  end
+
+  #####
+  # GenServer implementation
+
   def handle_call(:pop, _from, [ head | tail ]) do
     { :reply, head, tail }
   end
