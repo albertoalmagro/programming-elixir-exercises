16590d2f8bd2394d4e28e9151c65f095d5e522bf
Alberto Almagro <alberto.almagro@rakuten.com>
2017-07-19 18:33:12 +0200

    Add some tests

--- otp-app/stack/test/stack_test.exs
+++ otp-app/stack/test/stack_test.exs
@@ -2,7 +2,12 @@ defmodule StackTest do
   use ExUnit.Case
   doctest Stack
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "first element to pop is 5" do
+    assert Stack.Server.pop == 5
+  end
+
+  test "pushes new elements first in the stack" do
+    :ok = Stack.Server.push "dog"
+    assert Stack.Server.pop == "dog"
   end
 end
